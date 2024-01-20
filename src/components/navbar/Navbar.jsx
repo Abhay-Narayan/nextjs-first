bcb37fdaa8701f1125c987bcf6d58d69b4a58150
AbhayETH <abhaynarayan16122002@gmail.com>
2024-01-20 22:42:30 +0530

    added new files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import Link from "next/link"
-
+import Links from "./links/Links"
+import styles from './navbar.module.css'
 
 export const Navbar = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>NEXT</Link>
       <div>
-        BLOG
-      </div>
-      <div>
-        <Link href='/about'>About</Link>
+        <Links/>
       </div>
     </div>
   )
